971570716df7c5b033bcc5eb8980381ffb935169
Mia Morrell <52986587+mcmorre@users.noreply.github.com>
2023-12-01 11:58:40 -0500

    Create banded.py

---
+++ banded.py
@@ -0,0 +1,36 @@
+# fast solver for banded matrices from Newman
+def banded(Aa,va,up,down):
+
+    # Copy the inputs and determine the size of the system
+    A = copy(Aa)
+    v = copy(va)
+    N = len(v)
+
+    # Gaussian elimination
+    for m in range(N):
+
+        # Normalization factor
+        div = A[up,m]
+
+        # Update the vector first
+        v[m] /= div
+        for k in range(1,down+1):
+            if m+k<N:
+                v[m+k] -= A[up+k,m]*v[m]
+
+        # Now normalize the pivot row of A and subtract from lower ones
+        for i in range(up):
+            j = m + up - i
+            if j<N:
+                A[i,j] /= div
+                for k in range(1,down+1):
+                    A[i+k,j] -= A[up+k,m]*A[i,j]
+
+    # Backsubstitution
+    for m in range(N-2,-1,-1):
+        for i in range(up):
+            j = m + up - i
+            if j<N:
+                v[m] -= A[i,j]*v[j]
+
+    return v
